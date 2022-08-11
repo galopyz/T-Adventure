aab079284046f677765219d2cac467354617c641
Juhyun Suh <shipalnomoo@gmail.com>
2022-08-11 10:15:48 -0500

    Use api

--- src/routes/mongo.js
+++ src/routes/mongo.js
@@ -1,28 +1,26 @@
 import clientPromise from '$lib/mongodb-client';
 
-// export async function POST(request) {
-//   try {
-//     const dbConnection = await clientPromise;
-//     const db = dbConnection.db();
-//     const collection = db.collection('plots');
-//     const plot = JSON.parse(request.body);
-//     const newPlot = await collection.insertOne(plot);
-//     return {
-//       status: 200,
-//       body: {
-//         newPlot,
-//       },
-//     };
-//   } catch (err) {
-//     console.error(err);
-//   }
-// }
+const dbConnection = await clientPromise;
+const db = dbConnection.db('t-adventure');
+const collection = db.collection('plots');
+
+export async function POST({ request }) {
+  try {
+    const body = await request.json();
+    const newPlot = await collection.insertOne(body);
+    return {
+      status: 201,
+      body: {
+        newPlot,
+      },
+    };
+  } catch (err) {
+    console.error(err);
+  }
+}
 
 export async function GET() {
   try {
-    const dbConnection = await clientPromise;
-    const db = dbConnection.db('t-adventure');
-    const collection = db.collection('plots');
     const plots = await collection.find().toArray();
     return {
       status: 200,
@@ -30,6 +28,27 @@ export async function GET() {
         plots,
       },
     };
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export async function DELETE({ request }) {
+  try {
+    const option = await request.json();
+    console.log(option);
+    const plots = await collection.deleteOne(option);
+    if (plots.deleteCount === 1) {
+      return {
+        status: 201,
+        body: plots,
+      };
+    } else {
+      return {
+        status: 400,
+        body: 'delete not successful',
+      };
+    }
   } catch (err) {
     console.error(err);
   }
